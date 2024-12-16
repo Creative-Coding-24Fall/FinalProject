d1dfaa996eb2bad68e490ec887220eec56f0c37f
Shuangu <wshuang661@gmail.com>
2024-12-15 21:22:35 -0500

    1

--- sketch.js
+++ sketch.js
@@ -1,7 +1,45 @@
+const positiveEmojis = ["😀", "😄", "🥳", "😍", "😊", "😌", "🤩", "😋", "😎", "👍"];
+const negativeEmojis = ["😞", "😡", "😢", "😭", "😰", "😖", "😩",  "🤬", "🙃"];
+const neutralEmojis = ["😐", "😑", "😶", "🤔", "😬", "😴", "🤷", "🤨"];
+let allEmojis=[
+  positiveEmojis,negativeEmojis,neutralEmojis
+]
+let emojis=[]
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth,windowHeight);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  if(frameCount%10==0){
+    emojis.push(new Emotion(width,random(height)))
+  }
+  for(let i=0;i<emojis.length;i++){
+    emojis[i].display()
+    emojis[i].update()
+  }
+}
+class Emotion
+{
+  Emotion(x,y){
+    this.x=x
+    this.y=y
+    this.emojiType=int(random(3))
+    this.emoji=random(allEmojis[this.emojiType])
+    this.velx=-3
+    
+  }
+display(){
+  push()
+  translate(this.x,this.y)
+  textSize(30)
+  textAlign(CENTER,CENTER)
+  text(this.emoji,0,0)
+  pop()
+}
+update(){
+this.x+=this.velx
+
+}
+
 }
